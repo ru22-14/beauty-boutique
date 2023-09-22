5082419331051977f105d0c465f8433d274fa5ad
rohina usman <rohina.usman@gmail.com>
2023-09-22 15:49:21 +0000

    added data to json files and admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,2 +1,10 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+admin.site.register(Product)
+admin.site.register(Category)
+
+
 
 
--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    verbose_name_plural = 'Categories'
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    application = models.TextField()
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    Available_products =  models.IntegerField(default=15)
+
+    def __str__(self):
+        return self.name        
