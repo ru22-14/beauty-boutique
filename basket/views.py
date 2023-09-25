ec252a712e41c101bd2adcaa7677286cc92d1708
rohina usman <rohina.usman@gmail.com>
2023-09-25 21:33:41 +0000

    created basket and add to basket functionality

--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 
 # Create your views here.
 def view_basket(request):
     """View that renders basket page content"""
 
-    return render(request, 'basket/basket.html')
+    return render(request, 'basket/basket.html',)
+
+def add_to_basket(request, item_id):
+    """ Add quantity of the desired product to the basket """  
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url') 
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {bag[item_id]}'))
+    else:
+        bag[item_id] = quantity
+        # messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url) 
 
--- beauty_boutique/settings.py
+++ beauty_boutique/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by all auth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -154,6 +155,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 30
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
