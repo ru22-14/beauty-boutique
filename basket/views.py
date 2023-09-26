9a9915e74cb6655912806d778ed1e200b1c2fc7f
rohina usman <rohina.usman@gmail.com>
2023-09-26 11:00:49 +0000

    context processor updated

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 
+
 # Create your views here.
 def view_basket(request):
     """View that renders basket page content"""
@@ -11,18 +12,17 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url') 
-    bag = request.session.get('bag', {})
+    basket = request.session.get('basket', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-        messages.success(request,
-                             (f'Updated {product.name} '
-                              f'quantity to {bag[item_id]}'))
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+        # messages.success(request,
+        #                      (f'Updated {product.name} '
+        #                       f'quantity to {bag[item_id]}'))
     else:
-        bag[item_id] = quantity
+        basket[item_id] = quantity
         # messages.success(request, f'Added {product.name} to your bag')
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
+    request.session['basket'] = basket
     return redirect(redirect_url) 
 
