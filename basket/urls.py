731c8500a49ff9ec2425b1d7f0f6082e5555e8e0
rohina usman <rohina.usman@gmail.com>
2023-09-26 23:04:54 +0000

    update product functionality in bag is added

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('update/<item_id>/', views.update_basket, name='update_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
 
 
 # Create your views here.
@@ -18,11 +20,33 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
         # messages.success(request,
         #                      (f'Updated {product.name} '
-        #                       f'quantity to {bag[item_id]}'))
+        #                       f'quantity to {basket[item_id]}'))
     else:
         basket[item_id] = quantity
-        # messages.success(request, f'Added {product.name} to your bag')
+        # messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url) 
+
+def update_basket(request, item_id):
+    """ Adjust the products in the basket """  
+
+    quantity = int(request.POST.get('quantity')) 
+    basket = request.session.get('basket', {})          
+
+    if quantity > 0:
+
+        basket[item_id] = quantity
+                    # messages.success(request,
+                    #                     (f'Updated {product.name} '
+                    #                      f'quantity to {basket[item_id]}'))
+    else:
+        basket.pop(item_id)
+                    # messages.success(request,
+                    #                      (f'Removed {product.name} '
+                    #                       f'from your basket'))
+
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))     
 
