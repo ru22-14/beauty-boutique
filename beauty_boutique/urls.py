444ee3be2cce8ae618932027fc00dbde33b47d0e
rohina usman <rohina.usman@gmail.com>
2023-10-04 12:30:56 +0000

    added checkout template

--- beauty_boutique/urls.py
+++ beauty_boutique/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('favourites/', include('favourites.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
