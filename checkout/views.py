08c100428678a78e191f06e9c704dace80979525
rohina usman <rohina.usman@gmail.com>
2023-10-06 14:54:14 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form' : order_form,
+        'stripe_public_key' : 'pk_test_51NyEiYAsQ5JfzwVK98O2Np6lisSOw0oFDd89RbafwygcAnp78uIY3paB7hBm7rPLDHBrbd9qWndlvfIpCYxyZxwI00heAlNrYf',
+        'client_secret' : 'test client secret',
     }
     return render(request, template, context)
 
