b882ec368a400f13351b69ca0e9b978802856b68
rohina usman <rohina.usman@gmail.com>
2023-10-08 21:50:04 +0000

    added added webhooks for payment intent

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_successful/<order_number>', views.checkout_successful, name='checkout_successful'),
+    path('wh/', webhook, name='webhook'),
 ]
